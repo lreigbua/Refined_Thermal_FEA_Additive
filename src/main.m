9781816e194dfa172dab795cf0f77ce62e0c01e1
lreigbua <luis.reig-buades@strath.ac.uk>
2023-05-11 14:43:07 +0100

    Update main.m

--- src/main.m
+++ src/main.m
@@ -7,10 +7,10 @@ cd D:\mkb21147\Abaqus\Macro_Models\Process_Structure_FEA_SLM_w_refinement\Proces
 
 %Run Python Script to generate refined meshes for all layers:
 system('abaqus cae noGUI=../src/Set_up_adaptive_mesh_refinement.py')
-total_number_of_layers=2;
+total_number_of_layers=read_number_of_layers();
 
 % for current_layer=1:1:total_number_of_layers
-for current_layer=1:1:2
+for current_layer=1:1:total_number_of_layers
     delete *.lck
     Process_Generate_toolpath_and_steps = Generate_Toolpath_Event_Series_refinement_class();
     Process_Generate_toolpath_and_steps.read_input_file()
@@ -33,4 +33,22 @@ for current_layer=1:1:2
         status = system(command)
     end
 
+end
+
+function total_number_of_layers = read_number_of_layers()
+%Calculates total number of layers using data from updated json file
+    code=[
+            "import json"
+            "with open('D:\mkb21147\Abaqus\Macro_Models\Process_Structure_FEA_SLM_w_refinement\Process_Structure_FEA_SLM_w_refinement\data\jsonData.json', 'r') as myfile:"
+            "    data=myfile.read()"
+            "obj = json.loads(data)"
+            "out=obj"
+        ];
+
+        %transform python dict to matlab struct
+        dict_output = pyrun(code,'out');
+        struct_output = struct(dict_output);
+        
+        component_dimensions = cellfun(@double,cell(struct_output.component_dimensions));
+        total_number_of_layers = component_dimensions(3)/struct_output.layer_thickness;
 end
