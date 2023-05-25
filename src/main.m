4eb7ceedcf4170065f0514c3d0aba160fea152bc
lreigbua <luis.reig-buades@strath.ac.uk>
2023-05-25 14:07:48 +0100

    Update main.m

--- src/main.m
+++ src/main.m
@@ -2,25 +2,31 @@ clc
 close all
 clear variables
 
-%Go to data folder
+%change directory to output folder
 current_script_Path = matlab.desktop.editor.getActiveFilename;
 folder_of_current_script_Path = current_script_Path(1:end-6);
 cd(folder_of_current_script_Path);
 addpath(folder_of_current_script_Path);
-cd ..\data\
+cd ../input/
+
+input_file_struct = read_input_file();
+mkdir("../"+input_file_struct.output_folder_name)
+cd("../"+input_file_struct.output_folder_name)
 
 %%
-% % Run Python Script to generate refined meshes for all layers:
-system('abaqus cae noGUI=../src/Set_up_adaptive_mesh_refinement.py')
-total_number_of_layers=read_number_of_layers();
+% Run Python Script to generate refined meshes for all layers:
+% system('abaqus cae noGUI=../src/Set_up_adaptive_mesh_refinement.py')
+
 
 %% Run Abaqus Jobs
 copyfile ../src/INP_default.inp ./INP.txt
 
-
+data_file_struct = read_data_file();
+total_number_of_layers=data_file_struct.total_number_of_layers;
 Process_Generate_toolpath_and_steps = Generate_Toolpath_Event_Series_refinement_class();
 Process_Generate_toolpath_and_steps.read_input_file()
-for current_layer=40:1:40
+
+for current_layer=15:1:15
 % for current_layer=1:1:total_number_of_layers
     delete *.lck
     Process_Generate_toolpath_and_steps.current_layer=current_layer;
@@ -42,7 +48,7 @@ for current_layer=40:1:40
 
 end
 
-function total_number_of_layers = read_number_of_layers()
+function data_file_struct = read_data_file()
 %Calculates total number of layers using data from updated json file
     code=[
             "import json"
@@ -57,5 +63,24 @@ function total_number_of_layers = read_number_of_layers()
         struct_output = struct(dict_output);
         
         component_dimensions = cellfun(@double,cell(struct_output.component_dimensions));
-        total_number_of_layers = component_dimensions(3)/struct_output.layer_thickness;
+        data_file_struct.total_number_of_layers = component_dimensions(3)/struct_output.layer_thickness;
+
+end
+
+function input_file_struct = read_input_file()
+%Calculates total number of layers using data from updated json file
+    code=[
+            "import json"
+            "with open('.\input_file.json', 'r') as myfile:"
+            "    data=myfile.read()"
+            "obj = json.loads(data)"
+            "out=obj"
+        ];
+
+        %transform python dict to matlab struct
+        dict_output = pyrun(code,'out');
+        struct_output = struct(dict_output);
+        
+        input_file_struct.output_folder_name = string(struct_output.output_folder_name);
+
 end
