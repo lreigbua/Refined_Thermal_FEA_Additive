b3ebc9bed25ac58834b2bfe1cf5df9f4b815fbb5
lreigbua <luis.reig-buades@strath.ac.uk>
2024-04-26 11:44:47 +0100

    modified examples

--- examples/cube_1mm/run_1mm_cube.py
+++ examples/cube_1mm/run_1mm_cube.py
@@ -9,11 +9,11 @@ from Refined_Thermal_FEA_Additive import Abaqus_PBF_micro_thermal_class
 #Create simulation object, which reads the input json file
 Simulation = Abaqus_PBF_micro_thermal_class("./inputs/1mm_cube.json")
 
-# ##Generate scanpath from mtt file (only if libSLM with translators installed)
-# Simulation.Generate_scanpath_from_mtt()
+##Generate scanpath from mtt file (only if libSLM with translators installed)
+Simulation.Generate_scanpath_from_mtt()
 
-# ##Run Simulation
-# Simulation.run(Generate_meshes= False, from_layer=1, until_layer="end")
+##Run Simulation
+Simulation.run(Generate_meshes= True, from_layer=1, until_layer="end")
 
 ###Post Process
 #Generate video
